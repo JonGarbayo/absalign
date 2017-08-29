5f881585a9d37c73e4f2089f1445e1611dc0ff8c
Jon Garbayo <jon.garbayo@gmail.com>
2017-08-29 19:48:20 +0200

    Created a _class-collection.js file. The idea is to don't generate this list on each page load.

---
+++ src/_class-collection.js
@@ -0,0 +1,17 @@
+window.absalignClasses =
+[
+    ".abs-left-middle",
+    ".abs-center-top",
+    ".abs-center-middle",
+    ".abs-center-bottom",
+    ".abs-right-middle",
+    ".fix-left-middle",
+    ".fix-center-top",
+    ".fix-center-middle",
+    ".fix-center-bottom",
+    ".fix-right-middle",
+    ".abs-center",
+    ".abs-middle",
+    ".fix-center",
+    ".fix-middle"
+];

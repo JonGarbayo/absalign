174e5323f7a307882e63ca41d6d9685a69070b95
Jon Garbayo <jon.garbayo@gmail.com>
2017-09-10 12:54:11 +0200

    Moved the absalign classes generation to the Grunt process.

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,6 +23,372 @@ module.exports = function(grunt)
         'copy:demo_tmp_static'
     ]);
 
+    grunt.registerTask('generateAbsalignClasses', 'A task to dynamically create absalign classes for the JS polyfill.', function ()
+    {
+    // Declaring parts of classes names
+        var positions =
+        [
+            'abs',
+            'fix'
+        ];
+
+        var xAxes =
+        [
+            'left',
+            'center',
+            'right'
+        ];
+
+        var yAxes =
+        [
+            'top',
+            'middle',
+            'bottom'
+        ];
+
+        var pseudos =
+        [
+            false,
+            'before',
+            'after'
+        ];
+
+    // Declaring classes lists
+    // Lists by position
+        var absClasses = [];
+        var fixClasses = [];
+
+        var transformedClasses =
+        [
+            // '.absalign-animate'    // All absalign-animate classes are transformed
+        ];
+
+
+    // Axes lists
+        var leftTopClasses = [];
+        var leftMiddleClasses = [];
+        var leftBottomClasses = [];
+
+        var centerTopClasses = [];
+        var centerMiddleClasses = [];
+        var centerBottomClasses = [];
+
+        var rightTopClasses = [];
+        var rightMiddleClasses = [];
+        var rightBottomClasses = [];
+
+
+    // Mono-axis lists
+        var leftClasses = [];
+        var centerClasses = [];
+        var rightClasses = [];
+
+        var topClasses = [];
+        var middleClasses = [];
+        var bottomClasses = [];
+
+
+    // Inanimate classes lists
+        var leftBottomAnimateClasses = [];
+
+        var centerBottomAnimateClasses = [];
+
+        var rightTopAnimateClasses = [];
+        var rightMiddleAnimateClasses = [];
+        var rightBottomAnimateClasses = [];
+
+        var rightAnimateClasses = [];
+        var bottomAnimateClasses = [];
+
+
+        var currentClass = '';
+        var currentInanimateClass = '';
+
+    // First loop creates the positions and axes lists
+        positions.forEach(function (position)
+        {
+            xAxes.forEach(function (xAxis)
+            {
+                yAxes.forEach(function (yAxis)
+                {
+                    pseudos.forEach(function (pseudo)
+                    {
+                        is__inanimate = (xAxis === 'right' || yAxis === 'bottom');
+
+                        if (position !== 'fix' || !pseudo)
+                        {
+                            currentClass = '.' + position + '-' + xAxis + '-' + yAxis;
+
+        					if (pseudo)
+        					{
+        						currentClass = currentClass + '-' + pseudo;
+        					}
+
+        					if (is__inanimate)
+                            {
+                                currentInanimateClass = currentClass + '.absalign-animate';
+
+                                if (pseudo)
+                                {
+                                    currentInanimateClass = currentInanimateClass + '-' + pseudo + ':' + pseudo;
+                                }
+                            }
+
+                            if (pseudo)
+                            {
+                                currentClass = currentClass + ':' + pseudo;
+                            }
+                            else if (xAxis === 'center' || yAxis === 'middle')
+                            {
+                                transformedClasses.push(currentClass);
+                            }
+
+                            if (position === 'abs')
+                            {
+                                absClasses.push(currentClass);
+                            }
+                            else if (position === 'fix')
+                            {
+                                fixClasses.push(currentClass);
+                            }
+
+                            if (xAxis === 'left')
+                            {
+                                if (yAxis === 'top')
+                                {
+                                    leftTopClasses.push(currentClass);
+                                }
+                                if (yAxis === 'middle')
+                                {
+                                    leftMiddleClasses.push(currentClass);
+                                }
+                                if (yAxis === 'bottom')
+                                {
+                                    leftBottomClasses.push(currentClass);
+
+                                    if (is__inanimate)
+                                    {
+                                        leftBottomAnimateClasses.push(currentInanimateClass);
+                                    }
+                                }
+                            }
+
+                            if (xAxis === 'center')
+                            {
+                                if (yAxis === 'top')
+                                {
+                                    centerTopClasses.push(currentClass);
+                                }
+                                if (yAxis === 'middle')
+                                {
+                                    centerMiddleClasses.push(currentClass);
+                                }
+                                if (yAxis === 'bottom')
+                                {
+                                    centerBottomClasses.push(currentClass);
+
+                                    if (is__inanimate)
+                                    {
+                                        centerBottomAnimateClasses.push(currentInanimateClass);
+                                    }
+                                }
+                            }
+
+                            if (xAxis === 'right')
+                            {
+                                if (yAxis === 'top')
+                                {
+                                    rightTopClasses.push(currentClass);
+
+                                    if (is__inanimate)
+        							{
+        								rightTopAnimateClasses.push(currentInanimateClass);
+        							}
+                                }
+                                if (yAxis === 'middle')
+                                {
+                                    rightMiddleClasses.push(currentClass);
+
+                                    if (is__inanimate)
+        							{
+        								rightMiddleAnimateClasses.push(currentInanimateClass);
+        							}
+                                }
+                                if (yAxis === 'bottom')
+                                {
+                                    rightBottomClasses.push(currentClass);
+
+                                    if (is__inanimate)
+        							{
+        								rightBottomAnimateClasses.push(currentInanimateClass);
+        							}
+                                }
+                            }
+                        }
+                    });
+                });
+            });
+        });
+
+    // Second loop creates the mono-axis lists
+        positions.forEach(function (position)
+        {
+            xAxes.forEach(function (xAxis)
+            {
+                pseudos.forEach(function (pseudo)
+                {
+                    is__inanimate = (xAxis === 'right');
+
+                    if (position !== 'fix' || !pseudo)
+                    {
+                        currentClass = '.' + position + '-' + xAxis;
+
+        				if (pseudo)
+        				{
+        					currentClass = currentClass + '-' + pseudo;
+        				}
+
+        				if (is__inanimate)
+                        {
+                            currentInanimateClass = currentClass + '.absalign-animate';
+
+                            if (pseudo)
+                            {
+                                currentInanimateClass = currentInanimateClass + '-' + pseudo + ':' + pseudo;
+                            }
+                        }
+
+                        if (pseudo)
+                        {
+                            currentClass = currentClass + ':' + pseudo;
+                        }
+                        else if (xAxis === 'center')
+                        {
+                            transformedClasses.push(currentClass);
+                        }
+
+                        if (position === 'abs')
+                        {
+                            absClasses.push(currentClass);
+                        }
+                        else if (position === 'fix')
+                        {
+                            fixClasses.push(currentClass);
+                        }
+
+                        if (xAxis === 'left')
+                        {
+                            leftClasses.push(currentClass);
+                        }
+                        if (xAxis === 'center')
+                        {
+                            centerClasses.push(currentClass);
+                        }
+                        if (xAxis === 'right')
+                        {
+                            rightClasses.push(currentClass);
+
+                            if (is__inanimate)
+                            {
+                                rightAnimateClasses.push(currentInanimateClass);
+                            }
+                        }
+                    }
+                });
+            });
+
+            yAxes.forEach(function (yAxis)
+            {
+                pseudos.forEach(function (pseudo)
+                {
+                    is__inanimate = (yAxis === 'bottom');
+
+                    if (position !== 'fix' || !pseudo)
+                    {
+                        currentClass = '.' + position + '-' + yAxis;
+
+                        if (pseudo)
+        				{
+        					currentClass = currentClass + '-' + pseudo;
+        				}
+
+        				if (is__inanimate)
+                        {
+                            currentInanimateClass = currentClass + '.absalign-animate';
+
+                            if (pseudo)
+                            {
+                                currentInanimateClass = currentInanimateClass + '-' + pseudo + ':' + pseudo;
+                            }
+                        }
+
+                        if (pseudo)
+                        {
+                            currentClass = currentClass + ':' + pseudo;
+                        }
+                        else if (yAxis === 'middle')
+                        {
+                            transformedClasses.push(currentClass);
+                        }
+
+                        if (position === 'abs')
+                        {
+                            absClasses.push(currentClass);
+                        }
+                        else if (position === 'fix')
+                        {
+                            fixClasses.push(currentClass);
+                        }
+
+                        if (yAxis === 'top')
+                        {
+                            topClasses.push(currentClass);
+                        }
+                        if (yAxis === 'middle')
+                        {
+                            middleClasses.push(currentClass);
+                        }
+                        if (yAxis === 'bottom')
+                        {
+                            bottomClasses.push(currentClass);
+
+                            if (is__inanimate)
+                            {
+                                bottomAnimateClasses.push(currentInanimateClass);
+                            }
+                        }
+                    }
+                });
+            });
+        });
+
+    // Formatting the output
+        var output = "";
+
+        output += "_classesCollection =\n";
+        output += "[\n";
+
+        transformedClasses.forEach(function (className, i)
+        {
+            output += '\t"' + className + '"';
+
+            if (i < transformedClasses.length - 1)
+            {
+                output += ',';
+            }
+
+            output += "\n";
+        });
+
+        output += "];";
+
+    // Outputting in the _class-collection.js file
+        grunt.file.write(
+            'src/_class-collection.js',
+            output
+        );
+    });
+
 // buildAll: build package and demo
     grunt.registerTask('buildAll',
     [
@@ -30,6 +396,7 @@ module.exports = function(grunt)
         'compass',
         'css_important:dist',
         'postcss:dist',
+        'generateAbsalignClasses',
         'include_file:dist',
         'replace:dist',
 		'csscomb:dist',
@@ -51,6 +418,7 @@ module.exports = function(grunt)
 		'compass:dist',
         'css_important:dist',
         'postcss:dist',
+        'generateAbsalignClasses',
         'include_file:dist',
 		'replace:dist',
 		'csscomb:dist',
--- src/_class-collection.js
+++ src/_class-collection.js
@@ -1,17 +1,17 @@
 _classesCollection =
 [
-    ".abs-left-middle",
-    ".abs-center-top",
-    ".abs-center-middle",
-    ".abs-center-bottom",
-    ".abs-right-middle",
-    ".fix-left-middle",
-    ".fix-center-top",
-    ".fix-center-middle",
-    ".fix-center-bottom",
-    ".fix-right-middle",
-    ".abs-center",
-    ".abs-middle",
-    ".fix-center",
-    ".fix-middle"
+	".abs-left-middle",
+	".abs-center-top",
+	".abs-center-middle",
+	".abs-center-bottom",
+	".abs-right-middle",
+	".fix-left-middle",
+	".fix-center-top",
+	".fix-center-middle",
+	".fix-center-bottom",
+	".fix-right-middle",
+	".abs-center",
+	".abs-middle",
+	".fix-center",
+	".fix-middle"
 ];

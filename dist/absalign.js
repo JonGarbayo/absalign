8f94f6afadb5a65bca5a009e5e9308aabc100b3a
Jon Garbayo <jon.garbayo@gmail.com>
2018-03-18 22:57:53 +0100

    Rebuild the package

--- dist/absalign.js
+++ dist/absalign.js
@@ -1,5 +1,5 @@
 /*!
- * absalign v0.1.0 (JS part)
+ * absalign v0.2.0 (JS part)
  * Author: Jon Garbayo <jon.garbayo@gmail.com>
  * Repo: https://github.com/JonGarbayo/absalign
  * Licensed under MIT (https://github.com/JonGarbayo/absalign/blob/master/LICENSE)
@@ -86,8 +86,7 @@ if (!('indexOf' in Array.prototype))
 
 
 /**
- * Core of the polyfill. Scans the DOM to build the AbsalignElements
- * list.
+ * Core of the polyfill. Scans the DOM to build the AbsalignElements list.
  * @constructor
  */
 function AbsalignPolyfill()
@@ -97,10 +96,10 @@ function AbsalignPolyfill()
     var _classesCollection = [],
         _elements = [];
 
-    _populateClassCollection();
+    _populateClassesCollection();
 
     /**
-     * After instatiation, launch DOM operations.
+     * After instantiation, launch DOM operations.
      * @return {AbsalignPolyfill} AbsalignPolyfill after scanning the DOM.
      */
     OBJ.init = function ()
@@ -111,10 +110,18 @@ function AbsalignPolyfill()
     };
 
     /**
-     * Add the generated absalign classes from Grunt to the
-     * AbsalignPolyfill.
+     * Returns the absalign classes collection.
+     * @return {Array} absalign classes collection as array of strings.
      */
-    function _populateClassCollection()
+    OBJ.get__classesCollection = function ()
+    {
+        return _classesCollection;
+    };
+
+    /**
+     * Add the generated absalign classes from Grunt to the AbsalignPolyfill.
+     */
+    function _populateClassesCollection()
     {
         _classesCollection =
 [
@@ -137,20 +144,21 @@ function AbsalignPolyfill()
 
     /**
      * Scans the DOM using the absalign classes collection. A new
-     * AbsalignElement object is instantiated for each element found, and
-     * stored in the _elements collection.
+     * AbsalignElement object is instantiated for each element found, and stored
+     * in the _elements collection.
      */
     function _scan()
     {
         for (var i = 0; i < _classesCollection.length; i++)
         {
-            var className = _classesCollection[i];
-
-            var elements = document.getElementsByClassName(className);
+            var className = _classesCollection[i],
+                elements  = document.getElementsByClassName(className);
 
             for (var j = 0; j < elements.length; j++)
             {
-                _elements.push(new AbsalignElement(elements[j], className).init());
+                _elements.push(
+                    new AbsalignElement(elements[j], className, OBJ).init()
+                );
             }
         }
     }
@@ -159,55 +167,108 @@ function AbsalignPolyfill()
 }
 
 /**
- * Wraper object for every DOM element with an absalign class.
- * @param       {Object} element       DOM element.
- * @param       {String} absalignClass absalign class found on element
- *                                     to process.
+ * Wrapper object for every DOM element with an absalign class.
+ * @param {Object} element       DOM element.
+ * @param {String} absalignClass absalign class found on element to process.
+ *
  * @constructor
  */
-function AbsalignElement(element, absalignClass)
+function AbsalignElement(element, absalignClass, absalignPolyfill)
 {
     var OBJ = this;
 
-    var _element = element,
+    var _element       = element,
+        _absalignClass = absalignClass,
         _xAxis,
         _yAxis;
 
-    absalignClass = absalignClass.split('-');
-
-    var nbAxes = absalignClass.length - 1;
-
-// Mono axis case (i.e.: abs-left)
-    if (nbAxes === 1)
-    {
-        if (absalignUtilities().isXAxis(absalignClass[1]))
-        {
-            _xAxis = absalignClass[1];
-        }
-
-        if (absalignUtilities().isYAxis(absalignClass[1]))
-        {
-            _yAxis = absalignClass[1];
-        }
-    }
-
-// Double axis case (i.e.: abs-center-right)
-    if (nbAxes === 2)
-    {
-        _xAxis = absalignClass[1];
-        _yAxis = absalignClass[2];
-    }
+    _populate__axesFromAbsalignClass(_absalignClass);
 
     /**
-     * After instatiation, launch DOM operations.
+     * After instantiation, launch DOM operations.
      * @return {AbsalignElement} AbsalignElement after placing it.
      */
     OBJ.init = function ()
     {
         _place();
 
+        _on__classChange();
+
         return OBJ;
     };
+
+    /**
+     * Extract axes from the provided absalign class and populate _xAxis and
+     * _yAxis with it.
+     * @param {String} absalignClass absalign class of the element.
+     */
+    function _populate__axesFromAbsalignClass(absalignClass)
+    {
+        absalignClass = absalignClass.split('-');
+
+        var nbAxes = absalignClass.length - 1;
+
+        switch (nbAxes)
+        {
+        // No absalign class case (can happen only after class change)
+            case 0:
+                _xAxis = '';
+                _yAxis = '';
+                break;
+
+        // Mono axis case (i.e.: abs-left)
+            case 1:
+                if (absalignUtilities().is__XAxis(absalignClass[1]))
+                {
+                    _xAxis = absalignClass[1];
+                    _yAxis = '';
+                }
+
+                if (absalignUtilities().is__YAxis(absalignClass[1]))
+                {
+                    _xAxis = '';
+                    _yAxis = absalignClass[1];
+                }
+
+                break;
+
+        // Double axis case (i.e.: abs-center-right)
+            case 2:
+                _xAxis = absalignClass[1];
+                _yAxis = absalignClass[2];
+                break;
+        }
+    }
+
+    /**
+     * Extract the element absalign class from its class list.
+     * @return {String} The extracted class. Can be empty if absalign class has
+     *                  been removed.
+     */
+    function _fetch__absalignClassFromClass()
+    {
+        var classes      = _element.className.split(' '),
+            classes__it  = 0,
+            classes__len = classes.length,
+
+            absalignClassesCollection = absalignPolyfill.get__classesCollection();
+
+            absalignClass = '';
+
+    // Looping through element classes
+        while (absalignClass === '' && classes__it < classes__len)
+        {
+        // If current class is an absalign class, storing it
+            if (absalignClassesCollection.indexOf(classes[classes__it]) !== -1)
+            {
+                absalignClass = classes[classes__it];
+            }
+
+            classes__it++;
+        }
+
+        return absalignClass;
+    }
 
     /**
      * Main action of the polyfill: replace the transform() CSS function.
@@ -220,24 +281,54 @@ function AbsalignElement(element, absalignClass)
     {
         if (_xAxis === 'center')
         {
-            var elementWidth = _getWidth();
+            var elementWidth = _get__width();
 
             _element.style.marginLeft = -(elementWidth / 2) + 'px';
+        }
+        else
+        {
+            _element.style.marginLeft = '';
         }
 
         if (_yAxis === 'middle')
         {
-            var elementHeight = _getHeight();
+            var elementHeight = _get__height();
 
             _element.style.marginTop = -(elementHeight / 2) + 'px';
         }
+        else
+        {
+            _element.style.marginTop = '';
+        }
+    }
+
+    /**
+     * When the element class changes, update the element properties, and place
+     * it again.
+     * This behavior is powered by a custom function to detect class changes, as
+     * this event doesn't exist natively.
+     */
+    function _on__classChange()
+    {
+        var newAbsalignClass           = _fetch__absalignClassFromClass(),
+            is__absalignClassDifferent = (_absalignClass !== newAbsalignClass);
+
+        if (is__absalignClassDifferent)
+        {
+            _absalignClass = newAbsalignClass;
+
+            _populate__axesFromAbsalignClass(_absalignClass);
+            _place();
+        }
+
+        setTimeout(_on__classChange, 100);
     }
 
     /**
      * Get the element width.
      * @return {Number} The element width, as number.
      */
-    function _getWidth()
+    function _get__width()
     {
         return _element.offsetWidth;
     }
@@ -246,7 +337,7 @@ function AbsalignElement(element, absalignClass)
      * Get the element height.
      * @return {Number} The element height, as number.
      */
-    function _getHeight()
+    function _get__height()
     {
         return _element.offsetHeight;
     }
@@ -262,8 +353,8 @@ function absalignUtilities()
 {
     var OBJ = OBJ || { };
 
-    var _xPositions = ['left', 'center', 'right'];
-    var _yPositions = ['top', 'middle', 'bottom'];
+    var _xPositions = ['left', 'center', 'right'],
+        _yPositions = ['top', 'middle', 'bottom'];
 
     /**
      * From a position name, tell if it's from X axis or not.
@@ -271,7 +362,7 @@ function absalignUtilities()
      * @return {Boolean}         True if position is from the X axis, else
      *                           false.
      */
-    OBJ.isXAxis = function (position)
+    OBJ.is__XAxis = function (position)
     {
         return (_xPositions.indexOf(position) !== -1);
     };
@@ -282,14 +373,14 @@ function absalignUtilities()
      * @return {Boolean}         True if position is from the Y axis, else
      *                           false.
      */
-    OBJ.isYAxis = function (position)
+    OBJ.is__YAxis = function (position)
     {
         return (_yPositions.indexOf(position) !== -1);
     };
 
     /**
-     * Tell if the browser knows the transform CSS property.
-     * @return {Boolean} True if if the browser knows the transform CSS
+     * Tell if the browser supports the transform CSS property.
+     * @return {Boolean} True if the browser supports the transform CSS
      *                   property, else false.
      */
     OBJ.canUseTransform = function ()
@@ -322,5 +413,5 @@ function absalignUtilities()
 // Starting the polyfill
 if (absalignUtilities().canUseTransform() === false)
 {
-	var absalignPolyfill = new AbsalignPolyfill().init();
+	new AbsalignPolyfill().init();
 }
